f33bdef6d4f1092c86824efb2f23c240362454f1
Daniel Dyssegaard Kallick <ddyssegaardk@gmail.com>
2010-09-29 16:31:40 -0400

    made a board!

--- board.py
+++ board.py
@@ -1,9 +1,10 @@
 #while True:
-board = [[0]*7]*7
+board = [['a','b','c','d','e','f','g']]*7
 print board[1]
 print board[2]
 print board[3]
 print board[4]
 print board[5]
 print board[6]
-print board[7]
+x = raw_input("Where would you like to go? ")
+
