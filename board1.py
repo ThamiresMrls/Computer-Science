839f52c16f9df13929d315ed861e4065af137ec5
Daniel Dyssegaard Kallick <ddyssegaardk@gmail.com>
2010-09-30 17:49:43 -0400

    added some comments, made the vertical function work, complained about the diagonal functions not working, added comments.

--- board1.py
+++ board1.py
@@ -1,13 +1,9 @@
 #i=row
-#the idea here is to test for a winner, in the horizontal direction.
-#iterate through the rows, then go through the columns. if there are 
-#four in a row, break and make x the winner
-###
-###
-#so this right now works for any row, four in a row horizontally. 
-#sadly, it only works if the four in a row starts at a. Why? No
-#idea. It has a try except in it though!
-#added a comment.
+#e=column.
+#for horizontal.
+#Working.
+#All of the gameplay is working, but the tests are...
+#some are working, some arent. 
 def winnertest1():
     i=0
     while i<6:
@@ -18,42 +14,39 @@ def winnertest1():
         i+=1
     return 'on'
 #for vertical
-#basically the same as the first test
+#working.
 def winnertest2():
     e=0
-    while e<5:
-        for i in range(0,1):
-            try:
-                if board[i][e]=='x' and board[i+1][e]=='x' and board[i+2][e]=='x' and board[i+3][e]=='x':
-                    print "x wins!!!!"
-                    return 'over'
-                else:
-                    e+=1
-                    break
-            except ValueError:
-                pass
-
+    while e<7:
+        for i in range(3):
+            if board[i][e]=='X' and board[i+1][e]=='X' and board[i+2][e]=='X' and board[i+3][e]=='X':
+                print "X wins!!!!"
+                return 'over'
+        e+=1
+    return 'on'
 #for diagonal
+#not working! WHYYYY
 def winnertest3():
-    i=0
-    while i<4:
-        for e in range(0,3):
-            if board[i][e]=='x' and board[i+1][e+1]=='x' and board[i+2][e+2]=='x' and board[i+3][e+3]=='x':
-                print "x wins!!!!"
+    i=2
+    while i!=-1:
+        for e in range(4):
+            print i,e
+            if board[i][e]=='X' and board[i+1][e+1]=='X' and board[i+2][e+2]=='X' and board[i+3][e+3]=='X':
+                print "X wins!!!!"
                 return 'over'
-            else:
-                i+=1
-                break
+        i-=1
+    return 'on'
 #diagonal going other way
+#not working! WHYYY
 def winnertest4():
-    i=0
-    while i<4:
+    i=2
+    while i!=-1:
         for e in range(3,6):
-            if board[i][e]=='x' and board[i+1][e-1]=='x' and board[i+2][e-2]=='x' and board[i+3][e-3]=='x':
-                print "x wins!!!!"
+            if board[i][e]=='X' and board[i+1][e-1]=='X' and board[i+2][e-2]=='X' and board[i+3][e-3]=='X':
+                print "X wins!!!!"
                 return 'over'
-                break
-            i+=1
+        i+=1
+    return 'on'
 #prints the board.
 def print_board():
     print board[0]
@@ -63,6 +56,8 @@ def print_board():
     print board[4]
     print board[5]
 #create the board.
+#This could be modularized so you can define the size 
+#of the board, but why do that, really.
 board = [
 ['a','b','c','d','e','f','g'],
 ['a','b','c','d','e','f','g'],
@@ -106,10 +101,22 @@ def change_board(y,n,p):
         print "Sorry, that column is full"
         turn(p)
 print "Let's play connect 4!"
+#p is the player that is playing.
+#it's a string, either 'X' or 'O'
 def turn(p):
     print_board()
     print p,": It's your turn!"
     x = raw_input("Where would you like to go? ")
+    #check where the player wants to go. 
+    #if it's the a column, pass a to the drop 
+    #function. that will convert the column
+    #into a tuple, then pass that tuple, plus the player 
+    #variable, to change board, which will
+    #check how many pieces have been played 
+    #in that column already. then it changes 
+    #the board based on that. n is the 
+    #name of the column, and y is the number
+    #of the column.
     if x=='a':
         y,n=drop('a')
         change_board(y,n,p)
@@ -134,14 +141,14 @@ def turn(p):
     else:
         print "Sorry, that's not a column"
         turn(p)
-
 game='on'
-# game=winnertest1()
-#right now, just trying to get the first test to work.
+##############################
+#I know this is not a good way to 
+#implement it, but it's not the
+#point right now. --Daniel.
+#############################
 while not game=='over':
-    turn('x')
-    winnertest1()
-    game=winnertest1()
-    turn('o')
-    winnertest1()
-    game=winnertest1()
+    turn('X')
+    game=winnertest3()
+    turn('O')
+    game=winnertest3()
