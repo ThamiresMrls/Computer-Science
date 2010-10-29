1ef87b3ba7a29df87ae061ae8a842d751635df27
Daniel Dyssegaard Kallick <ddyssegaardk@gmail.com>
2010-10-28 22:34:36 -0400

    gpl

--- cannon.py
+++ cannon.py
@@ -1,7 +1,10 @@
+#This is a projectile motion program that determines the distance a 
+#projectile will go in ideal conditions, given initial velocity and 
+#angle of launch. By Daniel Dyssegaard Kallick October 28, 2010
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
 #the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
+#any later version.
 
 #This program is distributed in the hope that it will be useful,
 #but WITHOUT ANY WARRANTY; without even the implied warranty of
