fc77d5fd65b66990bab7a2cd020330c347a4b0c7
Daniel Dyssegaard Kallick <ddyssegaardk@gmail.com>
2010-11-03 18:41:35 -0400

    caesar ciphers solved

--- ceaser.py
+++ ceaser.py
@@ -1,59 +1,116 @@
+###THIS IS DANIEL'S CODE AND IT WORKS YEAAAAAAAAAAAH
+###DANIEL DYSSEGAARD KALLICK THAT IS.
 from letter_probabilities import letter_prob
 
-
 def decipher(ciphertext, n):
+    """
+    Takes in a ciphertext and a number, 'n'. Every letter is then
+    shifted n characters forward in the alphabet, modulo 26.
+    Capitals are dealt with. If it's not a letter, it's just tacked
+    on. (spaces and symbols are not shifted). Returns the enciphered
+    (or deciphered text).
+    """
     possible_plaintext=''
     for l in ciphertext:
-        possible_plaintext+=chr(ord(l)+n%26)
+        #lowercase
+        if ord(l)>96 and ord(l)<123:
+            #subtract 97 to get ord to be the posistion in the 
+            #alphabet, add n, modulo 26 it, add 97 again to get
+            #it back to the right ord for chr.
+            possible_plaintext+=chr((ord(l)-97+n)%26+97)
+        #uppercase
+        elif ord(l)>64 and ord(l)<91:
+            possible_plaintext+=chr((ord(l)-65+n)%26+65)
+        else:
+            possible_plaintext+=l
     return possible_plaintext
 
-def probability_list(possible_plaintext):
-    letter_count=[0]*27
+def probability_list(possible_plaintext,shift):
+    """
+    Takes in a phrase, returns a list of the
+    relative frequencies of each letter's appearance.
+    Capitals are treated as lowercase, and spaces and
+    symbols are ignored. the ouput list has the relative frequency of
+    a as the 0th value and the relative frequency of z as the 25th value.
+    """
+    letter_count=[0]*26
     for l in possible_plaintext:
-        if ord(l)==32:
-            letter_count[26]+=1
-        else:
+        if ord(l)>96 and ord(l)<123:
             letter_count[(ord(l)-97)]+=1
+        elif ord(l)>64 and ord(l)<91:
+            letter_count[(ord(l)-65)]+=1
 
     probablities=[0]*27
     n=0
-    while n<27:
-        probablities[n]=sum(letter_count)/float(letter_count[n])
+    while n<26:
+        #number of letter in question over total number of letters
+        probablities[n]=float(letter_count[n])/sum(letter_count)
+        n+=1
+    #keep track of which decryption it is (that is, how far it's shifted)
+    probablities[26]=shift
     return probablities
 
 
 def list_plaintexts(ciphertext):
+    """
+    takes in a ciphertext, returns a list
+    of all of the possible relative
+    letter frequency distributions
+    of all of the possible plaintexts
+    see also probability_list and decipher
+    """
     plaintexts=[[0]*27]*26
     for n in range(26):
-        plaintexts[n]=probability_list(decipher(ciphertext, n))
+        plaintexts[n]=probability_list(decipher(ciphertext, n),n)
     return plaintexts
 
 
-def compare(ideal, probability1, probability2):
+def compare(probability1, probability2):
+    """
+    Takes in two possible letter frequency distributions,
+    returns the one that deviates least from the
+    ideal english letter frequency distribution
+    """
+    #difference1 is the deviation from the ideal of 
+    #probability1, difference2 is the deviation from 
+    #the ideal of probability2.
     difference1=0
     difference2=0
-    for n in range(27):
+
+    #get the ideal
+    ideal=[0]*26
+    ctr=0
+    for l in 'abcdefghijklmnopqrstuvwxyz':
+        ideal[ctr]=letter_prob(l)
+        ctr+=1
+
+    for n in range(26):
+        #for each letter, see how far off it is from the ideal
+        #distribution of that letter, and add it to the total
+        #deviation for that decryption
         difference1+=abs(ideal[n]-probability1[n])
-    for n in range(27):
-        difference1+=abs(ideal[n]-probability2[n])
+    for n in range(26):
+        difference2+=abs(ideal[n]-probability2[n])
     if difference1>difference2:
-        return difference2
-    elif difference2>difference1:
-        return difference1
+        return probability2
+    else:
+        return probability1
 
 def best_match(ciphertext):
-    ideal=[0]*27
-    ctr=0
-    for l in 'abcdefghijklmnopqrstuvwxyz ':
-        ideal[ctr]=letter_prob(l)
-
+    """
+    Takes in a ciphertext, returns the most likely plaintext
+    """
     plaintexts=list_plaintexts(ciphertext)
     match=plaintexts[0]
     n=1
-    z=0
     while n<26:
-        match=compare(ideal, match, plaintexts[n])
-        z+=1
-phrase = raw_input("text here")
-print list_plaintexts(phrase)
-
+        match=compare(match, plaintexts[n])
+        n+=1
+    return match
+phrase = raw_input("text here\n")
+match_distribution = best_match(phrase)
+#in the 26th element of each distribution the shift of that distribution
+#is stored. This can then be used to decipher the text.
+shift=match_distribution[26]
+decryption=decipher(phrase, shift)
+print decryption
--- encipher.py
+++ encipher.py
@@ -1,10 +1,12 @@
 def encipher(plaintext, n):
     ciphertext=''
     for l in plaintext:
-        if ord(l)==32:
-            ciphertext+=l
+        if ord(l)>96 and ord(l)<123:
+            ciphertext+=chr((ord(l)-97+n)%26+97)
+        elif ord(l)>64 and ord(l)<91:
+            ciphertext+=chr((ord(l)-65+n)%26+65)
         else:
-            ciphertext+=chr(ord(l)+n%26)
+            ciphertext+=l
     return ciphertext
 
 n=int(raw_input("shift\n"))
