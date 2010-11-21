b8d7af33cc9ad1e31c88a15e4d16ce2475f5f532
Daniel Dyssegaard Kallick <ddyssegaardk@gmail.com>
2010-11-21 18:45:44 -0500

    on screen inputs, animations at the end

--- pygamehw.py
+++ pygamehw.py
@@ -53,7 +53,7 @@ def fire_gun(x,y,xvel,yvel,time_passed_seconds,player):
         if x>920 and x<1000 and y>520 and y<600:
             hit=True
     if player==2:
-        if x>0 and x<20 and y>520 and y<600:
+        if x>0 and x<80 and y>520 and y<600:
             hit=True
     return (int(x),int(y),inair,hit)
 
@@ -65,28 +65,56 @@ def make_vector(mag,angle):
     xvel=mag*math.cos(math.radians(angle))
     return (xvel,yvel)
 
-hp1=10
-hp2=10
-player=1
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            exit()
+def explode(x,y):
+    for n in range(17):
+        screen.blit(pygame.image.load("Explode"+str(n+1)+".png").convert_alpha(), (x,y))
+        pygame.display.update()
+        pygame.time.wait(33)
+        clear_screen(hp1,hp2,gravity)
 
-    #gravity = random.choice([random.uniform(0.2,1),random.uniform(1,5)])
-    gravity=1
+def get_input(player,variable,hp1,hp2,gravity):
+    user_input=''
+    typing=True
+    while typing==True:
+        for event in pygame.event.get():
+            if (event.type == KEYDOWN):
+                try:
+                    if chr(event.key).isdigit():
+                        user_input+=chr(event.key)
+                    else:
+                        if (event.key == K_ESCAPE):
+                            exit()
+                        if (event.key == K_RETURN) and len(user_input)>0:
+                            typing=False
+                        if (event.key == K_BACKSPACE):
+                            user_input=user_input[:-1]
+                        continue
+                except:
+                    continue
+        display_input = my_font.render("Player" + str(player) + variable + "? " + str(user_input), True, (0,0,0))
+        screen.blit(display_input, (450,300))
+        pygame.display.update()
+        clear_screen(hp1,hp2,gravity)
+    user_input=int(user_input)
+    return user_input
+
+hp1=1
+hp2=1
+player=1
+keepgoing=True
+while keepgoing==True:
+
+    gravity = random.choice([random.uniform(0.2,1),random.uniform(1,5)])
 
     clear_screen(hp1,hp2,gravity)
     pygame.display.update()
-    if hp1>0 and hp2>0:
-        magnitude=input("cannon power?\n")
-        angle=input("cannon angle?\n")
-        if angle<15:
-            print "Man, stop cheating"
-            continue
+
+    magnitude=get_input(player,' magnitude',hp1,hp2,gravity)
+    clear_screen(hp1,hp2,gravity)
+    angle=get_input(player,' angle',hp1,hp2,gravity)
+
     if player==2:
         angle = 180-angle
-
 
     vector=make_vector(magnitude,angle)
     xvel,yvel=vector
@@ -120,18 +148,19 @@ while True:
         player=2
         if hit==True and hp1>0 and hp2>0:
             hp2 -=1
-            for n in range(17):
-                screen.blit(pygame.image.load("Explode"+str(n+1)+".png").convert_alpha(), (920,520))
-                pygame.display.update()
-                pygame.time.wait(50)
-                clear_screen(hp1,hp2,gravity)
+            explode(920,520)
     elif player==2:
         player=1
         if hit==True and hp1>0 and hp2>0:
             hp1 -=1
-            for n in range(17):
-                screen.blit(pygame.image.load("Explode"+str(n+1)+".png").convert_alpha(), (0,520))
-                pygame.display.update()
-                pygame.time.wait(50)
-                clear_screen(hp1,hp2,gravity)
+            explode(0,520)
+    if hp1==0 or hp2==0:
+        explode(100,100)
+        explode(800,100)
+        explode(100,300)
+        explode(800,300)
+        keepgoing=False
+
     pygame.display.update()
+pygame.exit()
+exit()
