5b4def012e7d7f1945c38225dff6d00ff4c0033a
Daniel Dyssegaard Kallick <ddyssegaardk@gmail.com>
2010-11-20 21:33:55 -0500

    animation

--- pygamehw.py
+++ pygamehw.py
@@ -15,6 +15,10 @@ pygame.display.set_caption("Hello, World!")
 sprite = pygame.image.load(sprite_image_filename).convert_alpha()
 sprite2 = pygame.image.load('sprite2.jpg').convert()
 gravity = 1
+def clear_screen():
+    screen.fill((240,200,200))
+    screen.blit(sprite, (0,520))
+    screen.blit(sprite2, (920,520))
 def fire_gun(x,y,xvel,yvel,time_passed_seconds):
     distance_moved_x = (time_passed_seconds * xvel)
     distance_moved_y = (time_passed_seconds * yvel)
@@ -39,12 +43,8 @@ while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             exit()
-
-    screen.fill((240,200,200))
-    screen.blit(sprite, (0,520))
-    screen.blit(sprite2, (920,520))
+    clear_screen()
     pygame.display.update()
-
     magnitude=input("cannon power?\n")
     angle=input("cannon angle?\n")
     vector=make_vector(magnitude,angle)
@@ -69,6 +69,7 @@ while True:
         pos=fire_gun(x,y,vector[0],yvel,time_passed_seconds)
         pygame.draw.circle(screen,color,(pos[0],pos[1]),10)
         pygame.display.update()
+        clear_screen()
         inair=fire_gun(x,y,vector[0],yvel,time_passed_seconds)[2]
     if player==1:
         player=2
