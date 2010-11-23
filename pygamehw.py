c0543d894a876daffc6719c2c46d8b8b214631fc
Daniel Dyssegaard Kallick <ddyssegaardk@gmail.com>
2010-11-22 22:47:32 -0500

    made changing the size possible

--- pygamehw.py
+++ pygamehw.py
@@ -4,22 +4,24 @@ Author: Daniel Dyssegaard Kallick
 Description:tanks, or something like that
 '''
 
-background_image_filename = 'bg.jpg'
-sprite_image_filename = 'mouse.png'
 import pygame
 from pygame.locals import *
 import random
 from sys import exit
 import math
+import random
+background_image_filename = 'bg.jpg'
+sprite_image_filename = 'mouse.png'
+WIDTH = 1000
+HEIGHT = 600
 
 pygame.init()
 
 my_font = pygame.font.Font("linux-libertine.ttf", 16)
-screen = pygame.display.set_mode((1000,600), 0 ,32)
+screen = pygame.display.set_mode((WIDTH,HEIGHT), 0 ,32)
 pygame.display.set_caption("Hello, World!")
 tank1 = pygame.image.load(sprite_image_filename).convert_alpha()
 tank2 = pygame.image.load('sprite2.jpg').convert()
-import random
 
 def clear_screen(hp1,hp2,gravity):
     """
@@ -31,22 +33,22 @@ def clear_screen(hp1,hp2,gravity):
         winner='2'
     if hp2==0:
         winner='1'
-    screen.fill((240,200,200))
-    screen.blit(tank1, (0,520))
-    screen.blit(tank2, (920,520))
+    screen.fill((255,255,200))
+    screen.blit(tank1, (0,HEIGHT-80))
+    screen.blit(tank2, (WIDTH-80,HEIGHT-80))
     display_hp1 = my_font.render("Player1 HP: " + str(hp1), True, (0,0,0))
     display_hp2 = my_font.render("Player2 HP: " + str(hp2), True, (0,0,0))
     display_gravity = my_font.render("Gravity: " + str(round(gravity,3)), True, (0,0,0))
     display_winner = my_font.render("Player " + str(winner) + " Wins!", True, (0,0,0))
     screen.blit(display_hp1, (0,25))
-    screen.blit(display_hp2, (880,25))
-    screen.blit(display_gravity, (450,25))
+    screen.blit(display_hp2, (WIDTH-120,25))
+    screen.blit(display_gravity, ((WIDTH/2)-50,25))
     #make a blockage
-    pygame.draw.rect(screen,(0,0,0),Rect(400,400,200,200))
+    pygame.draw.rect(screen,(0,0,0),Rect((WIDTH/2)-100,HEIGHT-200,200,200))
     #if there's a winner...
     if winner!=None:
         #...show the winner
-        screen.blit(display_winner, (450,200))
+        screen.blit(display_winner,((WIDTH/2)-50,200))
 def fire_gun(x,y,xvel,yvel,time_passed_seconds,player):
     """
     Fires the cannon. Returns a posistion, whether or not
@@ -61,18 +63,18 @@ def fire_gun(x,y,xvel,yvel,time_passed_seconds,player):
     x+=distance_moved_x
     y+=distance_moved_y
     #if the ball hits a wall or the ground, it's not in the air
-    if y>595 or x>1000 or x<0:
+    if y>HEIGHT-5 or x>WIDTH or x<0:
         inair=False
     else:
         inair=True
     #if player one hits player two, hit is true
     if player==1:
-        if x>920 and x<1000 and y>520 and y<600:
+        if x>WIDTH-80 and x<WIDTH and y>HEIGHT-80 and y<HEIGHT:
             hit=True
     if player==2:
-        if x>0 and x<80 and y>520 and y<600:
+        if x>0 and x<80 and y>HEIGHT-80 and y<HEIGHT:
             hit=True
-    if x>400 and y>400 and x<600:
+    if x>(WIDTH/2)-100 and y>HEIGHT-200 and x<(WIDTH/2)+100:
         obstacle=True
         inair=False
     return (int(x),int(y),inair,hit,obstacle)
@@ -101,7 +103,7 @@ def explode(x,y):
         pygame.time.wait(33)
         clear_screen(hp1,hp2,gravity)
 
-def get_input(player,variable,hp1,hp2,gravity):
+def get_input(player,variable,hp1,hp2,ravity):
     user_input=''
     typing=True
     while typing==True:
@@ -121,16 +123,18 @@ def get_input(player,variable,hp1,hp2,gravity):
                 except:
                     continue
         display_input = my_font.render("Player" + str(player) + variable + "? " + str(user_input), True, (0,0,0))
-        screen.blit(display_input, (450,300))
+        screen.blit(display_input, ((WIDTH/2)-50,300))
         pygame.display.update()
         clear_screen(hp1,hp2,gravity)
     user_input=int(user_input)
     return user_input
 
-hp1=1
-hp2=1
+#set hitpoints
+hp1=5
+hp2=5
 player=1
 keepgoing=True
+#main loop
 while keepgoing==True:
 
     gravity = random.choice([random.uniform(0.2,1),random.uniform(1,5)])
@@ -149,12 +153,12 @@ while keepgoing==True:
     xvel,yvel=vector
 
     if player==1:
-        y=519
+        y=HEIGHT-81
         x=80
         color=(255,0,0)
     else:
-        y=519
-        x=920
+        y=HEIGHT-81
+        x=WIDTH-80
         color=(0,0,255)
     inair=True
     hit=False
