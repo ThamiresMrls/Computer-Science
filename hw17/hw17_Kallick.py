d735810746dc308b199f4d579fa1f4e897e3dd41
Daniel Dyssegaard Kallick <ddyssegaardk@gmail.com>
2010-12-02 19:07:40 -0500

    fixed

--- hw17/hw17_Kallick.py
+++ hw17/hw17_Kallick.py
@@ -15,19 +15,14 @@ length=len(wordlist)
 for n in range(26):
     fout=open(chr(n+97)+'.txt','w')
     #while the first letter of the word we are writing is correct
-    while wordlist[i][0]==chr(n+97):
-        #write the word
-        fout.write(wordlist[i])
-        #if we are not at the end of the file
-        try:
+    try:
+        while wordlist[i][0]==chr(n+97):
+            #write the word
+            fout.write(wordlist[i])
             #go to the next word
             i+=1
-        except:
-            pass
-        #if we are at the end of the file
-        else:
-            #stop
-            break
+    #except when we're at the end of the file
+    except:
+        pass
     fout.close()
-t2=time.time()
-print t2-t1
+print time.time()-t1
